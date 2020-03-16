b0ec071d3954db5c4e4aa8dbf02ab8d56882b265
Александр Мяснянкин <developlord@gmail.com>
2020-03-16 11:25:21 +0300

    finished index.html

---
+++ source/js/init-carousel.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+  $(".slider1").owlCarousel({
+    items: 1,
+    nav: true,
+    navText: ["", ""],
+    dots: false
+  });
+});
+
+$(document).ready(function () {
+  $(".slider2").owlCarousel({
+    items: 4,
+    nav: true,
+    navText: ["", ""],
+    dots: false,
+    margin: 25
+  });
+});
--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,15 @@
-'use strict';
+var tablinks = document.querySelectorAll('.tablinks');
+var tabcontent = document.querySelectorAll('.tabcontent');
 
-$(document).ready(function () {
-  $('.slick-slider').slick({
-    
+var makeActive = function (index) {
+  tablinks[index].addEventListener('click', function () {
+    document.querySelectorAll('.tablinks').forEach(n => n.classList.remove('active'));
+    this.classList.toggle('active');
+    document.querySelectorAll('.tabcontent').forEach(n => n.classList.remove('active'));
+    tabcontent[index].classList.toggle('active');
   });
-});
+};
+
+for (var i = 0; i < tablinks.length; i++) {
+  makeActive(i);
+}
